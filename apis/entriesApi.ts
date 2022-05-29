46ac2ed3c2b9c2f862890aca5fcec71c73b1b4e9
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-05-29 20:44:27 -0300

    First commit jira app

--- apis/entriesApi.ts
+++ apis/entriesApi.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const entriesApi= axios.create({
     baseURL:'/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      }
 })
 
 export default entriesApi;
